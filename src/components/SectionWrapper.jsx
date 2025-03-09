c4b132d58673ed905ed63afce60db8fac079f520
Rieona Fernandes <rxf323@student.bham.ac.uk>
2025-03-09 19:20:46 +0000

    Added animation and tweaked the design

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -13,15 +13,15 @@ const SectionWrapper = (Props) => {
     ) : (
       <div>
         <span className="uppercase text-blue-500">{title[0]}</span> {title[1]}
-        <span className="uppercase text-orange-500">{title[2]}</span> {title[3]}
+        <span className="uppercase text-orange-700">{title[2]}</span> {title[3]}
       </div>
     );
 
   return (
     <section id={id} className="min-h-screen flex flex-col gap-10">
       <div className="bg-slate-950 py-10 flex flex-col gap-2 justify-center items-center">
-        <p className="uppercase font-medium">{header}</p>
-        <h2 className="font-semibold text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
+        <p className="uppercase font-medium text-center">{header}</p>
+        <h2 className="font-semibold text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center">
           {titleData}
         </h2>
       </div>
